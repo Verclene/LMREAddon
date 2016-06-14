98b3fc6b4b07f7b596d7ebf59363b4a284459457
Verclene <xdev58bo@gmail.com>
2016-06-15 01:48:54 +0900

    1.0.0.1: Initial commit

---
+++ src/main/java/net/blacklab/lmr/entity/mode/EntityMode_Builder.java
@@ -0,0 +1,24 @@
+package net.blacklab.lmr.entity.mode;
+
+import net.blacklab.lmr.entity.EntityLittleMaid;
+import net.minecraft.entity.ai.EntityAITasks;
+
+public class EntityMode_Builder extends EntityModeBase {
+
+	public EntityMode_Builder(EntityLittleMaid pEntity) {
+		super(pEntity);
+	}
+
+	@Override
+	public int priority() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+	@Override
+	public void addEntityMode(EntityAITasks pDefaultMove, EntityAITasks pDefaultTargeting) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
